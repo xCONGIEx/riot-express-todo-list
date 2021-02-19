e8ab47696217a3bf619c7a5a53c0baf26b10ce40
Connor Gorr <xcongiex@gmail.com>
2021-02-19 10:36:32 -0800

    deploy.js update to host address

--- deploy.js
+++ deploy.js
@@ -85,7 +85,7 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '54.196.147.15',
+      host: '54.234.189.127',
       username: 'ubuntu',
       privateKey: 'L01HandsOnkey.pem'
     })
