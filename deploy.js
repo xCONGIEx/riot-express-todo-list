265895af6065c1846a886f419ed64c8aed2666cc
Connor Gorr <xcongiex@gmail.com>
2021-02-23 15:54:51 -0800

    server address update and testforsuccess correction

--- deploy.js
+++ deploy.js
@@ -85,7 +85,7 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '54.234.189.127',
+      host: '54.221.112.28',
       username: 'ubuntu',
       privateKey: 'L01HandsOnkey.pem'
     })
